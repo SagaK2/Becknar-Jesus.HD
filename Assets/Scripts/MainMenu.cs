a2e46496a12928739cac89a6847b95829cde1bf9
SagaK2 <Saga.Karlsson2@elev.ga.lbs.se>
2021-03-09 11:39:34 +0100

    Påbörjat menyn
    Jag har gjort det synliga men inte funktionerna på knapparna. Skrivit kod men måste lägga det till knapparna ännu.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+    public void Back()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+    public void PlayGame()
+    {
+        //Så att man kan loada fler scener än en specifik scen tex scen 1
+        SceneManager.LoadScene(1);
+        if (SceneManager.sceneCount == 1)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+    public void QuitGame()
+    {
+        //Avslutar spelet när den har "Byggts då"
+        Application.Quit();
+
+    }
+}
