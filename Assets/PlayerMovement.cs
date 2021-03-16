5ec754ed81b32324f8558ba4a7debb662103b703
TheoW04 <theo.wadsworth@elev.ga.lbs.se>
2021-03-16 10:09:43 +0100

    Gjort spelet halvt spelbar
    addat in maincharacter spriten och gjort så han kan hoppa och en stubbe som är animerad och rör sig

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float jumpforce;
+    public float moveinput;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.velocity = Vector2.up * jumpforce;
+        }
+    }
+}
